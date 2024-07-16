f0c81e7ffd60da8b124a6c969ccefe2b2b375796
sulfurmagic <johnaul81@gmail.com>
2024-07-16 14:46:50 +0300

    removed an unnessesary test

--- src/test/java/CardDeliveryTest.java
+++ src/test/java/CardDeliveryTest.java
@@ -8,8 +8,6 @@ import java.time.format.DateTimeFormatter;
 
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.*;
-import static com.codeborne.selenide.WebDriverRunner.url;
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.openqa.selenium.Keys.*;
 
 class CardDeliveryTest {
@@ -32,25 +30,6 @@ class CardDeliveryTest {
         $(".notification__content")
                 .shouldBe(Condition.visible, Duration.ofSeconds(15))
                 .shouldHave(text("Встреча успешно забронирована на " + date));
-
-    }
-
-    @Test
-    void ShouldFail_Name() {
-        Selenide.open("http://0.0.0.0:9999");
-        $("[data-test-id=city] input").setValue("Санкт-Петербург");
-        $("[data-test-id=date] input").doubleClick();
-        $("[data-test-id=date] input").sendKeys(BACK_SPACE);
-        String date = setDate(3, "dd.MM.yyyy");
-        $("[data-test-id=date] input").setValue(date);
-        $("[data-test-id=name] input").setValue("Latin Letters");
-        $("[data-test-id=phone] input").setValue("+70000000000");
-        $("[data-test-id=agreement]").click();
-        $(".button").click();
-        $("[data-test-id=name].input_invalid .input__sub")
-                .shouldHave(text("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы."));
-
-        assertEquals("http://0.0.0.0:9999/", url());
 
     }
 
